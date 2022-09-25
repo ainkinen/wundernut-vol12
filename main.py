df8622a0d2a784980f2898b910a7d37414c8faef
Antti-Jussi Inkinen <aj.inkinen@gmail.com>
2022-09-26 00:29:13 +0300

    Extract solver from the entry script

--- main.py
+++ main.py
@@ -1,20 +1,8 @@
-from src.crypto_utils import rot5_decrypt
-from src.img_utils import extract_img_data
-from src.ocr import extract_paragraph
-from src.segmenting import segment
+from src.solution import solve
 
 
 def main():
-    img_data = extract_img_data('parchment.png')
-
-    extracted_text = extract_paragraph(img_data)
-    print(f'{extracted_text=}')
-
-    decrypted_text = rot5_decrypt(extracted_text)
-    print(f'{decrypted_text=}')
-
-    segmented_text = segment(decrypted_text)
-    print(f'{segmented_text=}')
+    solve("parchment.png")
 
 
 if __name__ == "__main__":
